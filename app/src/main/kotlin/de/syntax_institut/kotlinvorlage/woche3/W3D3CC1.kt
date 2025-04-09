4c8c057d8ad490f1f2a05a7e7639833edfbdd307
tyfnnn <tayfun.i@icloud.com>
2025-04-09 10:33:46 +0200

    W3D3CC1

---
+++ app/src/main/kotlin/de/syntax_institut/kotlinvorlage/woche3/W3D3CC1.kt
@@ -0,0 +1,68 @@
+package de.syntax_institut.kotlinvorlage.woche3
+
+import android.R.id.message
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+
+@Composable
+fun GreetingComposable(modifier: Modifier = Modifier) {
+    var showGreeting by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        delay(2000L)
+
+        showGreeting = true
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (showGreeting) {
+            GreetingCard(message = "Hallo! Schön, dass du da bist!")
+        }
+    }
+}
+
+@Composable
+fun GreetingCard(message: String, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+    ) {
+        Text(
+            text = message,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingComposablePreview() {
+    GreetingComposable()
+}
